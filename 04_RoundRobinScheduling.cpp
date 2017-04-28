07fb30a51113d27a4d25209688d5a72e0579c795
Shivanshu Chaudhary <shivanshuchaudhary97@gmail.com>
2017-04-28 16:22:18 +0530

    Initial Commit

---
+++ 04_RoundRobinScheduling.cpp
@@ -0,0 +1,93 @@
+//RoundRobin Scheduling
+#include "stdio.h"
+
+int Process_no[20],Arrival_time[20],Burst_time[20],run_time[20],Priority[20],Job_status[20],completion_time[20],turn_around_time[20],wait_time[20],response_time[20];
+int num_processes;
+
+int suma(){
+  int retval=0;
+  for(int i=0;i<num_processes;i++)
+  {
+    retval+=Job_status[i];
+  }
+  return retval;
+}
+
+int main(){
+  int current_time=0;
+  int quanta = 1;
+  printf("\nEnter the Quanta :");
+  scanf("%d",&quanta);
+  printf("\nEnter The Number of Processes : ");
+  scanf("%d",&num_processes);
+  for(int i=0;i<num_processes;i++)
+  {
+    printf("\nEnter following for Process number %d",i+1);
+    printf("\nArrival Time : ");
+    scanf("%d",&Arrival_time[i]);
+    printf("\nBurst Time : ");
+    scanf("%d",&Burst_time[i]);
+
+    Job_status[i]=1;  //Implies Job unexecuted
+    run_time[i]=Burst_time[i];
+  }
+
+
+  printf("\nGAANT CHART=>\n");
+  while(suma()!=0){
+
+    int flag=0;
+    for(int i=0;i<num_processes;i++)
+    {
+      if(Job_status[i]==1){
+        if((flag==0)&&(Arrival_time[i]<=current_time)){
+          printf("\nProcess %d Executed",i+1);
+          flag =1;
+          if(quanta<run_time[i]){
+            run_time[i]-=quanta;
+            current_time+=quanta;
+          }
+          else{
+            current_time+=run_time[i];
+            completion_time[i]=current_time;
+            run_time[i]=0;
+            Job_status[i]=0;
+          }
+        }
+        else if(Arrival_time[i]<current_time){
+          printf("\nProcess %d Executed",i+1);
+          flag =1;
+          if(quanta<run_time[i]){
+            run_time[i]-=quanta;
+            current_time+=quanta;
+          }
+          else{
+            current_time+=run_time[i];
+            completion_time[i]=current_time;
+            run_time[i]=0;
+            Job_status[i]=0;
+          }
+        }
+
+
+      }
+    }
+    if(flag==0){
+      current_time++;
+    }
+}
+
+  for(int i=0;i<num_processes;i++){
+    turn_around_time[i]=completion_time[i]-Arrival_time[i];
+    wait_time[i] = turn_around_time[i]-Burst_time[i];
+    response_time[i]=wait_time[i];
+  }
+
+  for(int i=0;i<num_processes;i++){
+    printf("\nProcess Number\tArrival Time\tBurst Time\tCompletion Time\tTurn Around Time\tWait Time\tResponse Time\t");
+    printf("\n%d\t\t%d\t\t%d\t\t%d\t\t%d\t\t\t%d\t\t%d",i+1,Arrival_time[i],Burst_time[i],completion_time[i],turn_around_time[i],wait_time[i],response_time[i]);
+  }
+  printf("\n");
+
+  return 1;
+}
---
+++ ReadMe.md
@@ -0,0 +1,35 @@
+Group 1:
+
+1) WAP to implement Priority Queue scheduling Algorithm.[done]
+2) WAP to implement SJF non-preemptive Algorithm.[done]
+3) WAP to implement Priority Scheduling(Pre-Emptive).[done]
+4) WAP to implement RoundRobin Scheduling Algorithm.[done]
+5) WAP to implement Highest Response Ratio Next Algorithm.
+6) WAP to implement Multilevel Queue Scheduling.(4 Queues)
+NOTE: Print Completion Time, Turn Around Time, Wait Time and Response Time for Each of the above Algorithm.
+
+7) WAP to implement following memory Algorithm :
+    a) Best Fit Algorithm
+    b) Next Fit Algorithm
+    c) Worst Fit Algorithm
+8) WAP to implement Least Recently Used Page Algorithm.
+9) WAP to implement Optimal Page Replacement Algorithm.
+10)WAP to implement SCAN Elevator Algorithm & CSCAN Algorithm.
+
+Group 2:
+
+1) WAP to implement Priority Queue scheduling Algorithm.[done]
+2) WAP to implement SJF non-preemptive Algorithm.[done]
+3) WAP to implement Priority Scheduling(Pre-Emptive).[done]
+4) WAP to implement RoundRobin Scheduling Algorithm.[done]
+5) WAP to implement Highest Response Ratio Next Algorithm.
+6) WAP to implement Multilevel Queue Scheduling.(4 Queues)
+NOTE: Print Completion Time, Turn Around Time, Wait Time and Response Time for Each of the above Algorithm.
+
+7) WAP to implement following memory Algorithm :
+  a) Best Fit Algorithm
+  b) Next Fit Algorithm
+  c) Worst Fit Algorithm
+
+8) WAP to implement First In First Out Page Replacement Algorithm.
+9) WAP to implement SSTF and FCFS disk scheduling Algorithm.
